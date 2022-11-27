e8b6c70cce7966e0b130ebeff5f2391c380f9c43
Andrei Stanciu <andrei4.stanciu@gmail.com>
2022-11-27 14:56:39 +0200

    fixed jwt verif bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, process.env.SECRET || 'secret')
+        const decoded = jwt.verify(token, process.env.SECRET)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
         if (!user) {
@@ -15,6 +15,7 @@ const auth = async (req, res, next) => {
         req.user = user
         next()
     } catch (e) {
+        console.log(e.message)
         res.status(401).send({ error: 'Please authenticate.' })
     }
 }

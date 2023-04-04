6a04c2b6d61bde1ca6c6ada36b8813629b5fb2db
Andrei Stanciu <andrei4.stanciu@gmail.com>
2023-04-04 17:42:04 +0300

    another structure

--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
 const path = require('path')
 require("dotenv").config({ path: path.resolve(__dirname, './', '.env') });
-const express = require('express')
 require('./utils/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const http = require('http')
+const app = require('./app')
+const PORT = process.env.PORT || 3000
+const server = http.createServer(app)
 
-const app = express()
-const port = process.env.PORT || 3000
-
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+server.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`)
 })
-

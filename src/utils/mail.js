8501050338182177458f35c48d191ee441df7ba9
Andrei Stanciu <andrei4.stanciu@gmail.com>
2023-04-04 16:35:57 +0300

    added

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -2,18 +2,20 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-async function sendMail() {
+async function sendMail(mail) {
+    if (!mail) throw new Error('Error, not mail was provided')
     const msg = {
-        to: 'andrei4.stanciu@gmail.com', // Change to your recipient
-        from: 'test@example.com', // Change to your verified sender
+        to: mail, // Change to your recipient
+        from: 'andrei4.stanciu@gmail.com', // Change to your verified sender
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     }
     try {
         const mail = await sgMail.send(msg)
+        if (mail) console.log('Mail was sent')
     } catch (error) {
-
+        console.log(error)
     }
 }
 
